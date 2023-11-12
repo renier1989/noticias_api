db88aabbd4b1c40448f9fa94802361d0c49ae4d8
Renier Vargas <renier1989@gmail.com>
2023-11-12 10:04:29 -0500

    340. Consultando las Noticias según la categoría seleccionada

--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import axios from "axios";
 import { createContext, useEffect, useState } from "react";
 
 const NoticiasContext = createContext();
@@ -6,12 +7,19 @@ const NoticiasContext = createContext();
 const NoticiasProvider = ({children}) =>{
 
     const [categoria, setCategoria] = useState('general');
+    const [noticias, setNoticias] = useState([]);
     const handleChangeCategoria = e=> {
         setCategoria(e.target.value);
     }
 
     useEffect(()=>{
+        const consultarAPI = async() =>{
+            const url = `https://newsapi.org/v2/top-headlines?country=us&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`;
+            const {data} = await axios(url);
 
+            setNoticias(data.articles);
+        }
+        consultarAPI()
     },[categoria]);
 
     return (
